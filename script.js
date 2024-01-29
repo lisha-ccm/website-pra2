ccfc943ce34796fa2108bf8ce8cc15dbfdb7ad00
Lisha <lisha.subs.ccm@gmail.com>
2024-01-28 23:28:15 -0800

    site with nav

---
+++ script.js
@@ -0,0 +1,13 @@
+printNameArrow = (name) => {
+  console.log(name)
+}
+
+printNameArrow("lisha")
+
+let sumArrow = (a, b) => a + b
+
+console.log(sumArrow(7, 3))
+
+let printHiArrow = (name) => "Hi " + name
+
+console.log(printHiArrow("lisha "))
